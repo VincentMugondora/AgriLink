949416c28d80d702f52de723ab4888429aa6ad0b
vincentmugondora <vinmugondora@gmail.com>
2025-09-03 14:43:31 +0200

    updated

--- farmconnect_webapp/src/pages/Home.jsx
+++ farmconnect_webapp/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import Hero from '../components/home/Hero'
-import Features from '../components/home/Features'
 import HowItWorks from '../components/home/HowItWorks'
 import CTA from '../components/home/CTA'
 import StatsBar from '../components/home/StatsBar'
